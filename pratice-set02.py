bf518eeadb3cab8f5c96d63d4b21cd3f061b2840
Sahil071803 <sahilatram303@gmail.com>
2025-01-19 23:34:52 +0530

    Add this paragraph

---
+++ pratice-set02.py
@@ -0,0 +1,48 @@
+# # p1
+
+# a = 54
+# b = 45
+# c = a + b
+# print(c)
+
+
+
+# p2
+
+# a = 55
+
+# b = 4
+
+# print("Reminder of a is divided by b is:" , a % b)
+
+
+# p3
+# a = int(input("Enter the number 1:"))
+# b = int(input("Enter the number 2:"))
+
+# # print("Reminder of both number divided with each other : " ,a % b)
+
+# print("Value of number 1 is : ",a)
+# print("Value of number 2 is : ",b)
+
+# print("Reminder of both number divided with each other : " ,a % b)
+
+# p4
+# a = 34
+# b = 80
+# c = a >= b;
+# print(c)
+
+# p5
+
+# a = float(input("Enter the 1 number :"))
+# b = float(input("Enter the 2 number :"))
+
+# average = (a + b)/2;
+
+# print("The average between the two number is : ",average)
+
+# p6
+number = 5  # Assign a value to the variable
+square = number ** 2
+print(square)
