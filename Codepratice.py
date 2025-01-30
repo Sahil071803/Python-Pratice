5874f394f8d4066b3e07967405995b3b420f2a58
Sahil071803 <sahilatram303@gmail.com>
2025-01-30 23:35:01 +0530

    Added this code

--- Codepratice.py
+++ Codepratice.py
@@ -1,4 +1,4 @@
-print("Hello World")
+# print("Hello World")
 #  1)  datatypes
 
 # a = 50
@@ -14,61 +14,61 @@ print("Hello World")
 #  print(c)
 
 
-# operators
+# 2)operators
 # Artithmatic operators
-a = 15
-b = 4
-c = a+b
-print(c)
+# a = 15
+# b = 4
+# c = a+b
+# print(c)
 
 
-a = 54
-b = 6
-c = a / b
-print(c)
+# a = 54
+# b = 6
+# c = a / b
+# print(c)
 
 
-a = 54 
-t = type(a)
-print(t)
+# a = 54 
+# t = type(a)
+# print(t)
 
 
-# Assignment operators
-a = 2
-b = 4
-a += b
-print(a)
+# # Assignment operators
+# a = 2
+# b = 4
+# a += b
+# print(a)
 
-a = 5 
-b = 4.5
-a -= b
-print(type(a))
+# a = 5 
+# b = 4.5
+# a -= b
+# print(type(a))
 
-# Comparison opeartors
-a = 7
-b = 5
-c = (a == b)
-print(c)
+# # Comparison opeartors
+# a = 7
+# b = 5
+# c = (a == b)
+# print(c)
 
-a = 66
-b = 75
-c = a < b
-print(c)
+# a = 66
+# b = 75
+# c = a < b
+# print(c)
 
-x = 7
-y = 7
+# x = 7
+# y = 7
+# # c = x >= y
+# c = x != y
+# print(c)
+
+
+# x = 5.5
+# y = 4.5
 # c = x >= y
-c = x != y
-print(c)
+# print(type(c))
 
 
-x = 5.5
-y = 4.5
-c = x >= y
-print(type(c))
-
-
-#input Function
+# 3)input Function
 
 # a = input("Enter the number 1:")
 # b = input("Enter the number 2:")
@@ -96,44 +96,170 @@ print(type(c))
 
 
 
-#String 
+# 4)String 
 
 
 # a = "Sahil Atram"
 # print(a)
 
-y = "Dipak Chutiya Hyae"
-print(y)
+# y = "Dipak Chutiya Hyae"
+# print(y)
+
+# # s = "sahilatram"
+# # user = a.capitalize()
+# # print(user)
 
 # s = "sahilatram"
-# user = a.capitalize()
+# user = s.replace("atram","Atram")
 # print(user)
 
-s = "sahilatram"
-user = s.replace("atram","Atram")
-print(user)
+# Name = "Sahil"
+# print( len(Name))
 
-Name = "Sahil"
-print( len(Name))
-
-q = " earth is gola kon bola"
-print(q)
+# q = " earth is gola kon bola"
+# print(q)
 
 
+# 5)List & Tuples
+# List 
 
-flowers = ["Rose" , "Lily" ,"China-Rose", "SunFlower"]
+# flowers = ["Rose" , "Lily" ,"China-Rose", "SunFlower"]
+# # print(flowers)
+# # print(type(flowers))
+
+# flowers.extend(["hibiscus"])
 # print(flowers)
-# print(type(flowers))
-
-flowers.extend(["hibiscus"])
-print(flowers)
 
 
 
-students = ["Rose","Michle","Virat","Rohit","Suresh"]
+# students = ["Rose","Michle","Virat","Rohit","Suresh"]
 
-students.insert(["Rishab"],2)
-print(students)
+# print(students)
+
+# students.remove("Rose")
+# print(students)
+
+
+# students.insert(1,"Sahil")
+# print(students)
+
+# a = students.pop()
+# print(a)
+
+
+# position = students.index("Michle")
+# position = students.index("Virat")
+# print(position)
+
+# students.extend(["Rose"])
+# print(students)
+
+# students.extend(["Virat"])
+# print(students)
+
+# students.append("jack")
+# print(students)
+
+# students.clear()
+# students.sort()
+# students.reverse()
+# print(students)
+
+
+# numbers = [5, 2, 9, 1, 5, 6]
+# numbers.sort()
+# print(numbers) 
+
+
+
+# Tuples
+# Numbers = (10,20,30,40,50)
+# print(Numbers)
+
+# Food = ("Burger","Pizza","Ice-creame","Noddles")
+# # a = Food.index("Burger")
+# # a = Food.index("Ice-creame")
+# # a = Food.index("Noddles")
+# # a = Food.index("Pizza")
+
+# a = Food.count([0])
+# x = Food.index("Burger")
+# print(a)
+# print(x)
+
+
+# Concatenation
+# a = ("Sahil" , "Akhil")
+# b = ("Somnath", "Akash")
+# result = a + b
+# print(result)
+
+# # Repetition
+
+# a = (1,2,3)
+# Repetition = a*3
+# print(Repetition)
+
+# a = (1,2,3,4,6)
+# Repetition = a*10000
+# print(Repetition)
+
+# 6) Dictionary & Sets
+
+# Owners = {
+#     "Sahil" :{"Swift"},
+#     "Prathamesh" :{"Nano"},
+#     "Samyak" :{"Nexon"}
+# }
+
+# print(Owners)
+
+
+# info = {
+#     "Student" : "Sahil-Atram",
+#     "Rollno" : {54},
+#     "Dept" : "CSE(IOT)"
+# }
+
+# print(type(info))
+
+
+
+# print(info.keys())
+
+# print(info.values())
+
+
+# print(info.items())
+
+# Sets
+set1 = {1,2,3,3,4}
+print(set1)
+
+
+Rno = {54,55,49,74,74,99}
+print(type(Rno))
+
+
+a = {1,2,4,7}
+print(len(a))
+
+
+a = {5,7,8,1,6}
+b = {4,9,7,4,3,4}
+Add =a.union(b)
+print(Add)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Dis&set.py
@@ -0,0 +1,18 @@
+# Dictionary
+students = {
+    "Sahil" : "Goodguy",
+    "Prathamesh" : "Badboy",
+    "Priyanshu" : "Confusedboy",
+    "Samyak" : "Chilldude"
+
+}
+
+print(students)
+
+
+# Sets
+Numbers = {1,4,7,2,5,8}
+print(Numbers)
+
+s= sets()
+print(s)
